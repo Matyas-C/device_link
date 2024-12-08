94929fb9026bd7ba6682453d1ffec71831391076
matyas-c <matyas.caf@gmail.com>
2024-12-08 18:03:48 +0100

    rewrite UDP discovery into a single class

--- lib/udp_discovery.dart
+++ lib/udp_discovery.dart
@@ -8,10 +8,10 @@ import 'package:hive_ce/hive.dart';
 import 'other_device.dart';
 import 'message_type.dart';
 
-class UdpClient {
-  static final UdpClient _instance = UdpClient._internal();
-  factory UdpClient() => _instance;
-  UdpClient._internal();
+class UdpDiscovery {
+  static final UdpDiscovery _instance = UdpDiscovery._internal();
+  factory UdpDiscovery() => _instance;
+  UdpDiscovery._internal();
 
   final _deviceBox = Hive.box('device');
   late final String uuid;
@@ -23,11 +23,11 @@ class UdpClient {
   Future<void> initialize() async {
     uuid = _deviceBox.get('uuid');
     broadcastAddress = await NetworkInfo().getWifiBroadcast();
-    socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+    socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 8081);
     startListener(socket);
   }
 
-  Future<void> sendUdpDiscoveryBroadcast() async {
+  Future<void> sendDiscoveryBroadcast() async {
     final Map<String, dynamic> discoveryMessage = {
       'type': MessageType.discover,
       'uuid': uuid,
@@ -52,7 +52,7 @@ class UdpClient {
   }
 
   void startListener(RawDatagramSocket socket) {
-    socket.listen((RawSocketEvent event) {
+    socket.listen((RawSocketEvent event) async {
       if (event == RawSocketEvent.read) {
         Datagram? datagram = socket.receive();
         if (datagram != null) {
@@ -61,6 +61,19 @@ class UdpClient {
           if (decodedMessage['uuid'] == uuid) return;
 
           switch (decodedMessage['type']) {
+            case MessageType.discover:
+              final Map<String, dynamic> response = {
+                'type': MessageType.discoverResponse,
+                'uuid': uuid,
+                'version': '1.0',
+                'deviceType': determineDeviceType(),
+                'name': 'Device Name',
+                'ip': await NetworkInfo().getWifiIP(),
+              };
+              String jsonResponse = json.encode(response);
+              socket.send(utf8.encode(jsonResponse), datagram.address, datagram.port);
+              break;
+
             case MessageType.discoverResponse:
               final OtherDevice device = OtherDevice(
                 uuid: decodedMessage['uuid'],
@@ -68,8 +81,19 @@ class UdpClient {
                 name: decodedMessage['name'],
                 ip: decodedMessage['ip'],
               );
-              if (DiscoveredDevices.list.any((d) => d.uuid == device.uuid)) return; //ignoruje zarizeni pokud v seznamu jiz je (podle uuid)
+              if (DiscoveredDevices.list.any((d) => d.uuid == device.uuid)) return;
               onDeviceDiscovered(device);
+              break;
+
+            case MessageType.connectionRequest:
+              final Map<String, dynamic> response = {
+                'type': MessageType.connectionResponse,
+                'uuid': uuid,
+                'version': '1.0',
+                'wsAddress': 'ws://${await NetworkInfo().getWifiIP()}:8080',
+              };
+              String jsonResponse = json.encode(response);
+              socket.send(utf8.encode(jsonResponse), datagram.address, datagram.port);
               break;
 
             case MessageType.connectionResponse:
@@ -77,8 +101,7 @@ class UdpClient {
               break;
 
             default:
-              print('Unknown response type: ${decodedMessage['type']}');
-              break;
+              print('Unknown message type received');
           }
         }
       }
