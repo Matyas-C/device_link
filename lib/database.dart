08095587a6ca9e041496bd670f49a32003e76f9a
matyas-c <matyas.caf@gmail.com>
2024-12-23 01:22:28 +0100

    fix potential null pointer

--- lib/database.dart
+++ lib/database.dart
@@ -11,10 +11,10 @@ class DeviceBox {
   final _deviceBox = Hive.box('device');
 
   void initData() {
-    if (!_deviceBox.containsKey('uuid')) {
+    if (!_deviceBox.containsKey('uuid') || _deviceBox.get('uuid') == null) {
       _deviceBox.put('uuid', const Uuid().v4().toString());
     }
-    if (!_deviceBox.containsKey('name')) {
+    if (!_deviceBox.containsKey('name') || _deviceBox.get('name') == null) {
       _deviceBox.put('name', 'Zařízení');
     }
   }
